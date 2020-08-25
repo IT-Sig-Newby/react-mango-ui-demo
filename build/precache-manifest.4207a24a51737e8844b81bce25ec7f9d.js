30463a3d92d1c3a7ffdbcd9b8b21205da418e3a9
Arkana Protego <arkana.protego@gmail.com>
2020-08-25 21:43:32 +0100

    First commit

---
+++ build/precache-manifest.4207a24a51737e8844b81bce25ec7f9d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "779e040d6eddeefd8bf7fa06657e6362",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f040e6b40286b7baa0a6",
+    "url": "./static/css/main.00b0312e.chunk.css"
+  },
+  {
+    "revision": "12db11413eb326b943df",
+    "url": "./static/js/2.58d2405a.chunk.js"
+  },
+  {
+    "revision": "5514754ecebdf7a94ff8a01ae5993830",
+    "url": "./static/js/2.58d2405a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f040e6b40286b7baa0a6",
+    "url": "./static/js/main.f2046c34.chunk.js"
+  },
+  {
+    "revision": "67716aa1016085acbd63",
+    "url": "./static/js/runtime-main.21121d30.js"
+  }
+]);
